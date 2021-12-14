8ea5c057f7577b0333c6639044a1cc51ee5a67b9
AeroSyntax <daniel.ladendorfer@gmail.com>
2021-12-14 22:43:45 +0100

    Created StdWrapper
    This module defines a few discriminated unions which may share names with Kappa++ features

---
+++ KappaPP/src/aero/utils/StdWrapper.fs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------------------------------
+// Copyright (c) Ladendorfer Daniel.  
+// All Rights Reserved.  See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------
+namespace Aero.Utils
+
+///<summary>
+/// Contains discriminated unions for the standard library which may have conflicting names with Kappa++ features.
+///</summary>
+module StdWrapper =
+    // create discriminated unions for the System.Collections.Generic datatypes to minimize naming confusion
+    type SysStack<'a> = System.Collections.Generic.Stack<'a>
+    type SysList<'a> = System.Collections.Generic.List<'a>
+    type SysDict<'a, 'b> = System.Collections.Generic.Dictionary<'a, 'b>
+
